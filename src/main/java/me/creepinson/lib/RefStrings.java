d3c237053e624d2d86b969699ceb601463eccdf5
dafuq360 <theoplaysmc@gmail.com>
2017-06-13 18:52:03 -0700

    d

--- src/main/java/me/creepinson/lib/RefStrings.java
+++ src/main/java/me/creepinson/lib/RefStrings.java
@@ -2,7 +2,7 @@ package me.creepinson.lib;
 
 public class RefStrings {
 	public static final String MODID = "meepersplus";
-	public static final String VERSION = "1.0.0-Alpha";
+	public static final String VERSION = "0.0.1-Alpha";
 	public static final String CLIENTSIDE = "me.creepinson.main.ClientProxy";
 	public static final String SERVERSIDE = "me.creepinson.main.CommonProxy";
 
